205df1c40f67afcb3d00ac7d2ecc44166e7ec45f
seredonato <54689539+seredonato@users.noreply.github.com>
2023-04-24 12:22:44 -0300

    rename static variables

--- src/main/java/ConversorCelsiusFahrenheit.java
+++ src/main/java/ConversorCelsiusFahrenheit.java
@@ -8,29 +8,36 @@
  * @author Serena
  */
 public class ConversorCelsiusFahrenheit extends Conversor {
+    
+    private static final String NAME = "Celsius / Fahrenheit";
+    private static final String LABEL_1 = "Celsius";
+    private static final String LABEL_2 = "Fahrenheit";
+    private static final double CONVERT_FACTOR_1 = 9.0 / 5.0;
+    private static final double CONVERT_FACTOR_2 = 5.0 / 9.0;
+    private static final double CONVERT_OFFSET = 32.0;
 
     @Override
     public String getName() {
-        return "Celsius / Fahrenheit";
+        return NAME;
     }
 
     @Override
     public String getLabel1() {
-        return "Celsius";
+        return LABEL_1;
     }
 
     @Override
     public String getLabel2() {
-        return "Fahrenheit";
+        return LABEL_2;
     }
 
     @Override
     public Double convertValue2(Double celsius) {
-        return (celsius - 32) * 5 / 9;
+        return (celsius * CONVERT_FACTOR_1) + CONVERT_OFFSET;
     }
 
     @Override
     public Double convertValue1(Double fahrenheit) {
-        return (fahrenheit * 9 / 5) + 32;
+        return (fahrenheit - CONVERT_OFFSET) * CONVERT_FACTOR_2;
     }
 }
